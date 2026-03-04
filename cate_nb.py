93e4a508fb184dacefeb1cf8f85354207d3d5908
evsky <tubeakram7@gmail.com>
2026-03-04 21:16:21 +0200

    initial structure

---
+++ cate_nb.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+class NaiveBayesClassifier:
+    def __init__(self):
+        self.spam_prob = 0.5
+        self.notSpam_prob = 0.5
+        
+    def loadData(self):
+        df = pd.read_csv('data/spam_or_not_spam.csv')
+        return df
+
+    def compute_initial_probs(self):
+        data = self.loadData()
+        nrecords = len(data)
+        
+        counts = data['label'].value_counts()
+        spam_counts = counts[1]
+        not_spam_counts = counts[0]
+        
+        total = spam_counts + not_spam_counts
+        
+        self.spam_prob = spam_counts / total
+        self.notSpam_prob = not_spam_counts / total
+        
+        print(self.spam_prob, self.notSpam_prob)
+        return self.spam_prob, self.notSpam_prob
+        
+if __name__ == "__main__":
+    nbc = NaiveBayesClassifier()
+    
+    df = pd.read_csv('data/spam_or_not_spam.csv')
+    print(len(df['email'][0].strip()))
+    
---
+++ download_data.py
@@ -0,0 +1,4 @@
+import kagglehub
+
+path = kagglehub.dataset_download("ozlerhakan/spam-or-not-spam-dataset")
+print("Path to dataset files:", path)
