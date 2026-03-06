4fcd693efb822dc493446ebbc3dc6580fa0c7405
evsky <tubeakram7@gmail.com>
2026-03-06 23:52:43 +0200

    move code to notebook instead of py file

--- cate_nb.py
+++ cate_nb.py
@@ -1,34 +1,37 @@
-import pandas as pd
-import numpy as np
+### DEPRECATED ### 
 
-class NaiveBayesClassifier:
-    def __init__(self):
-        self.spam_prob = 0.5
-        self.notSpam_prob = 0.5
-        
-    def loadData(self):
-        df = pd.read_csv('data/spam_or_not_spam.csv')
-        return df
 
-    def compute_initial_probs(self):
-        data = self.loadData()
-        nrecords = len(data)
+# import pandas as pd
+# import numpy as np
+
+# class NaiveBayesClassifier:
+#     def __init__(self):
+#         self.spam_prob = 0.5
+#         self.notSpam_prob = 0.5
         
-        counts = data['label'].value_counts()
-        spam_counts = counts[1]
-        not_spam_counts = counts[0]
+#     def loadData(self):
+#         df = pd.read_csv('data/spam_or_not_spam.csv')
+#         return df
+
+#     def compute_initial_probs(self):
+#         data = self.loadData()
+#         nrecords = len(data)
         
-        total = spam_counts + not_spam_counts
+#         counts = data['label'].value_counts()
+#         spam_counts = counts[1]
+#         not_spam_counts = counts[0]
         
-        self.spam_prob = spam_counts / total
-        self.notSpam_prob = not_spam_counts / total
+#         total = spam_counts + not_spam_counts
         
-        print(self.spam_prob, self.notSpam_prob)
-        return self.spam_prob, self.notSpam_prob
+#         self.spam_prob = spam_counts / total
+#         self.notSpam_prob = not_spam_counts / total
         
-if __name__ == "__main__":
-    nbc = NaiveBayesClassifier()
+#         print(self.spam_prob, self.notSpam_prob)
+#         return self.spam_prob, self.notSpam_prob
+        
+# if __name__ == "__main__":
+#     nbc = NaiveBayesClassifier()
     
-    df = pd.read_csv('data/spam_or_not_spam.csv')
-    print(len(df['email'][0].strip()))
+#     df = pd.read_csv('data/spam_or_not_spam.csv')
+#     print(df['email'][0])
     
