e5b1c86c9a07c870c04e2d56e3d8e3a435aeb82e
evsky <tubeakram7@gmail.com>
2026-03-11 00:32:08 +0200

    Add feature encoding and PCA implementation to data preprocessing
    - Introduced `encode_features` function to encode categorical features using LabelEncoder.
    - Enhanced PCA class with type hints and methods for mean calculation, value shifting, covariance matrix computation, and eigenvalue decomposition.
    - Updated constructor to accept a DataFrame and store its values for further processing.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,11 +1,53 @@
 import numpy as np
+from pandas import DataFrame
+from sklearn.preprocessing import LabelEncoder
+
+def encode_features(data: DataFrame) -> DataFrame:
+    encoded_data = data.drop('class', axis=1).copy()
+    encoder = LabelEncoder()
+    
+    for col in encoded_data.columns:
+        encoded_data[col] = encoder.fit_transform(encoded_data[col])
+    
+    return encoded_data
 
 class PCA:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, df: DataFrame):
+        self.df: DataFrame = df
+        self.data_values = self.df.values
     
-    def process_data():
-        pass
+    def means(self):
+        means = np.mean(self.data_values, axis=0)
+        return means
+
+    def shift_values(self) -> np.ndarray:
+        data_values = self.data_values
+        means = self.means()
+        
+        shifted_values = data_values - means
+        return shifted_values
+    
+    def compute_cov_matrix(self):
+        shifted_values: np.ndarray = self.shift_values()
+        n = self.data_values.shape[0]
+        
+        cov_matirx = (1 / n) * shifted_values.T @ shifted_values
+        return cov_matirx
+    
+    
+    def compute_fprime(self, k=3):
+        cov_matrix = self.compute_cov_matrix()
+        eigenvals, eigenvectors = np.linalg.eigh(cov_matrix)
+        
+        sorted_idx = np.argsort(eigenvals)[::-1]
+        eigenvectors = eigenvectors[:, sorted_idx]
+        
+        shifted_values = self.shift_values()
+        
+        Q = eigenvectors[:, :k].T
+        F = shifted_values.T
+        
+        return Q @ F
 
 
 class FeatureSelection:
